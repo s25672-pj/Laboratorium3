1a7738aeec332bec6d6a2829b95aca43a1f182bd
s25672-pj <92215054+s25672-pj@users.noreply.github.com>
2021-11-18 17:39:11 +0100

    Add files via upload

---
+++ Zadanie3.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+    float x;
+    float y;
+    float suma, roznica, iloczyn;
+    float iloraz;
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> x;
+    cout << "Podaj druga liczbe: ";
+    cin >> y;
+    suma = x + y;
+    roznica = x - y;
+    iloczyn = x * y;
+    cout << "Suma podanych liczb to: " << setprecision(2) << suma << endl;
+    cout << "Roznica podanych liczb to: " << setprecision(2) << roznica << endl;
+    cout << "Iloczyn podanych liczb to: " << setprecision(2) << iloczyn << endl;
+    if (y == 0) {
+        cout << "Jako dzielnik podano liczbe 0, nie mozna wykonac dzielenia";
+    }
+    else {
+        iloraz = x / y;
+        cout << "Iloraz podanych liczb to: " << setprecision(2) << iloraz << endl;
+    };
+
+    return 0;
+
+}
