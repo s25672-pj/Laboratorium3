6c333cd7584057fae7ee678220b0b9dbe1ded78a
s25672-pj <92215054+s25672-pj@users.noreply.github.com>
2021-11-18 18:03:02 +0100

    Add files via upload

---
+++ Zadanie3.4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int C(int n, int k)
+{
+    if (k == 0 || k == n)
+        return 1;
+    else
+        return C(n - 1, k - 1) + C(n - 1, k);
+}
+
+int main()
+{
+    int wiersze;
+    cout << "Trojkat Pascala\n" << "Podaj liczbe wierszy: ";
+    cin >> wiersze;
+    
+    for (int n = 0; n <= wiersze; n++)
+    {
+        cout << setw(2) << n << setw(3 * (wiersze - n)) << "";
+        for (int k = 0; k <= n; k++)
+            cout << setw(6) << C(n, k);
+        cout << endl;
+    }
+    return 0;
+}
