0bf0a3238c3d9dadcf3b3fc129a8cbeed7d5c39b
s25672-pj <92215054+s25672-pj@users.noreply.github.com>
2021-11-18 17:44:44 +0100

    Add files via upload

---
+++ Zadanie3.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char znak;
+    cout << "Podaj jakis znak z klawiatury, az do momentu gdy trafisz poszukiwany: ";
+    cin >> znak;
+    while (znak != 't') {
+        cout << znak;
+        cin >> znak;
+
+
+    }
+    cout << "Podales poszukiwany znak t";
+    return 0;
+
+}
